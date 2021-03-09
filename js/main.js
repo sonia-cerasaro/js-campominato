77eda06c6c611f19d29bfbbd47be1a9ff2116e57
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-09 19:16:33 +0100

    seconda soluzione

--- js/main.js
+++ js/main.js
@@ -5,68 +5,130 @@
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-// Bonus (da fare solo se funziona tutto il resto)
-// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali: con difficoltà 0 => tra 1 e 100 con difficoltà 1 => tra 1 e 80 con difficoltà 2 => tra 1 e 50
 
 
 
+var MaxNumber = 100;
+var RecivedUserInputs = [];
+var BombNumbers = GetBombNumbers();
+var IsUserWinner = GetGameResult();
 
-// FUNCTIONS
-// Devo contare da 1 a 16 e inserire in array (numeri computer)
-// // generare un numero tra 1 e 100
-// // verifico che numero richiesto non e' in array
-// se in array verifico altrimenti richiedo numero.
-// contare da 1 a 100 - 16(array) perche' il computer ha generato 16 numeri.
-// verifico che numero 1 e' tra uno e 100
-// verifico che non bomba se bomba TRUE, termina ciclo.
-// Stampo punteggio
+PrintResult(IsUserWinner);
 
-var bombsArray = [];
-var safesArray = [];
-var maxNumber = 100;
-
-
-bombsArray = createBombs(bombsArray, maxNumber);
-
-var punteggio = game(bombsArray, safesArray, maxNumber);
-
-console.log(createBombs(bombsArray, 100));
-console.log("Game over!");
-console.log("Il tuo punteggio e':" + punteggio);
-
-
-function game(bombsArray, safesArray, maxNumber) {
-  while (safesArray.length < maxNumber - 16) {
-    var numeroUtente = parseInt(prompt("Inserisci un numero e prova a non morire"));
-    if (!isNaN(numeroUtente) && 1 <= numeroUtente && numeroUtente <= 100 && !safesArray.includes(numeroUtente)) {
-     if (!bombsArray.includes(numeroUtente)) {
-      safesArray.push(numeroUtente);
-    } else {
-      return safesArray.length;
-    }
-   }
+function PrintResult(isWinner){
+  if(!isWinner){
+    alert("Mi dispiace, hai perso! Hai trovato " + RecivedUserInputs.length + " numeri su 84");
   }
-  return safesArray.length;
+  else{
+    alert("Complimenti, hai vinto!");
+  }
 }
 
-// Ogni volta che inseriro' un elemento aumenta finche' arrivero' a 16 elementi
+function GetGameResult(){
+  var maxUserInput = MaxNumber - BombNumbers.length;
 
-function createBombs(array, max) {
-  while (array.length < 16) {
-    var numero = randomNumberInRange(1, 100)
-    if (!array.includes(numero)) {
-      array.push(numero);
+  for (var i = 0; RecivedUserInputs.length <= maxUserInput; i++) {
+    var userInput = parseInt(prompt("Inserisci un valore:      RIMANENTI - " + (maxUserInput - i)));
+    if (!isValidUserInput(userInput) || RecivedUserInputs.includes(userInput)){
+      alert("Inserisci un valore numerico valido compreso tra 1 e 100");
+      --i; //Serve per evitare che il contatore dei valori rimanenti da inserire cali nel caso l'utente inserisca un valore errato
+    }
+    else if(isBombNumber(userInput)){
+      return false;
+    }
+    else{
+      RecivedUserInputs.push(userInput);
     }
   }
-  return array;
+  return true;
 }
 
-// INSERISCO FUNZIONE RANDOMICA
+function isBombNumber(userInput){
+  if (!BombNumbers.includes(userInput)){
+    return false;
+  }
+  return true;
+}
 
-function randomNumberInRange(min, max) {
+function isValidUserInput(userInput){
+  if(isNaN(userInput) || userInput < 1 || userInput > 100){
+    return false;
+  }
+  return true;
+}
+
+function GetBombNumbers(){
+  var min = 1;
+  var bombNumbers = [];
+
+  for (var i = 0; bombNumbers.length < 16; i++) {
+    var randomNumber = GetRandomNumber(min, MaxNumber);
+    if(!bombNumbers.includes(randomNumber)){
+      bombNumbers[i] = randomNumber;
+    }
+  }
+  return bombNumbers;
+}
+
+function GetRandomNumber(min, max) {
   if (isNaN(min) || isNaN(max)) {
     console.log("Non sono numeri!");
   } else {
     return Math.floor(Math.random()* (max - min) + min);
   }
 }
+
+
+
+
+
+// -----------------------------------------------------------------
+// var bombsArray = [];
+// var safesArray = [];
+// var maxNumber = 100;
+//
+//
+// bombsArray = createBombs(bombsArray, maxNumber);
+//
+// var punteggio = game(bombsArray, safesArray, maxNumber);
+//
+// console.log(createBombs(bombsArray, 100));
+// console.log("Game over!");
+// console.log("Il tuo punteggio e':" + punteggio);
+//
+//
+// function game(bombsArray, safesArray, maxNumber) {
+//   while (safesArray.length < maxNumber - 16) {
+//     var numeroUtente = parseInt(prompt("Inserisci un numero e prova a non morire"));
+//     if (!isNaN(numeroUtente) && 1 <= numeroUtente && numeroUtente <= 100 && !safesArray.includes(numeroUtente)) {
+//      if (!bombsArray.includes(numeroUtente)) {
+//       safesArray.push(numeroUtente);
+//     } else {
+//       return safesArray.length;
+//     }
+//    }
+//   }
+//   return safesArray.length;
+// }
+//
+// // Ogni volta che inseriro' un elemento aumenta finche' arrivero' a 16 elementi
+//
+// function createBombs(array, max) {
+//   while (array.length < 16) {
+//     var numero = randomNumberInRange(1, 100)
+//     if (!array.includes(numero)) {
+//       array.push(numero);
+//     }
+//   }
+//   return array;
+// }
+//
+// // INSERISCO FUNZIONE RANDOMICA
+//
+// function randomNumberInRange(min, max) {
+//   if (isNaN(min) || isNaN(max)) {
+//     console.log("Non sono numeri!");
+//   } else {
+//     return Math.floor(Math.random()* (max - min) + min);
+//   }
+// }
